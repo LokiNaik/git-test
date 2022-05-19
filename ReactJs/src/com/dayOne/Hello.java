11e7d72073d5b06064915eb0580dffe13aea59f9
Lokesh <Lokesh@LOKESH-LAPTOP>
2022-05-19 22:11:13 +0530

    added text file

--- ReactJs/src/com/dayOne/Hello.java
+++ ReactJs/src/com/dayOne/Hello.java
@@ -4,7 +4,8 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.print("Hello World");
-
+		System.err.print("Hello World");
+		System.err.print("Hello World");
 	}
 
 }
